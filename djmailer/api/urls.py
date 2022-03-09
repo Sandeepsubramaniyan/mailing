8a2913e93a3c938d284b63bc02411e42342e2202
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-09 09:48:28 +0530

    using as_view method for appropriate request

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from .views import hello_world
+from .views import HelloWorldView
 
 urlpatterns = [
-    url(r'^hello',hello_world,name="hello_world")
+    url(r'^hello',HelloWorldView.as_view(),name="hello_world")
 ]
