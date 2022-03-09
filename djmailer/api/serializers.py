510b6db01e4a93dc08c54b60129b1656ec4422df
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-09 15:06:45 +0530

    removed unwanted import

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -1,6 +1,5 @@
-from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
 
 class HelloWorldSerializer(serializers.Serializer):
-    name=serializers.charfield(required=True, max_length=6)
-    age=serializers.IntegerField(required=False, min_value=10, default=10)
+    name = serializers.CharField(required=True, max_length=6)
+    age = serializers.IntegerField(required=False, min_value=10, default=10)
