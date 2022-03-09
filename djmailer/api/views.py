32c822fd564d5bcfce476dfda09101decb6bb449
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-09 09:49:32 +0530

    giving  input "name"

--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -6,6 +6,12 @@ class HelloWorldView(APIView):
     
     def get(self,request):
         return Response({"message": "hello world!"})
+    
+    def post(self,request):
+        name = request.data.get("name")
+        if not name:
+            return Response({"error":"No name passed"})
+        return Response({"message":"Hello {}!".format(name)})
      
 
 
