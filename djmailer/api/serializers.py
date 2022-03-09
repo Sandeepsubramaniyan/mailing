f9a820282cabc5cbcf64322f0c0c080b719298b8
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-09 16:50:57 +0530

    creating Meta for name to table and its fields

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Subscriber
 
 class SubscriberSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=50)
-    age = serializers.IntegerField()
-    email = serializers.EmailField()
+    class Meta:
+        model = Subscriber
+        fields  = "__all__"
+        
