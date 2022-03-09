3c58688e5caf8db2ad5b57f30d97256a9573400f
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-09 15:48:31 +0530

    changed the end point URL in urls.py

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from .views import HelloWorldView
+from .views import HelloWorldView, SubscriberView
 
 urlpatterns = [
-    url(r'^hello',HelloWorldView.as_view(),name="hello_world")
+    url(r'^subscriber',SubscriberView.as_view(),name="subscriber")
 ]
