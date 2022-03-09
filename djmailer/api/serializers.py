c17d2eec06d3363a5842cb377ea4b8f4cac358ff
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-09 16:46:35 +0530

    changed the class to ModelSerializer

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Subscriber
 
-class SubscriberSerializer(serializers.Serializer):
+class SubscriberSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=50)
     age = serializers.IntegerField()
     email = serializers.EmailField()
