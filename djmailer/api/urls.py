121073d856ed04888e54c6133a51f5afde792e9f
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-11 08:37:53 +0530

    added login function and auth and permission

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,7 +1,15 @@
 from rest_framework.routers import SimpleRouter
 from .views import SubscriberViewSet
+from .views import login
+from django.conf.urls import url
+
 
 router = SimpleRouter()
 router.register("subscribers",SubscriberViewSet)
 
-urlpatterns = router.urls
+
+
+urlpatterns = router.urls + [
+    
+    url(r'^login', login)
+]
--- djmailer/djmailer/urls.py
+++ djmailer/djmailer/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.conf.urls import url,include
-
 from api import urls as api_urls
+
+
 
 urlpatterns = [
     
     url(r'^api/',include(api_urls)),
     url(r'^admin/', admin.site.urls),
+    
 ]
