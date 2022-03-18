7d0eec6a12d9aab787d6da995778dcc2392f5d0b
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-18 11:12:37 +0530

    registeration

--- djmailer/api/models.py
+++ djmailer/api/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Subscriber(models.Model):
     name = models.CharField("Name",max_length=50)
     age = models.IntegerField("Age")
-    email = models.EmailField("Email")
+    email = models.EmailField("Email")   
     
+    
+
+    
+   
     
--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,15 +1,19 @@
 from rest_framework.routers import SimpleRouter
-from .views import SubscriberViewSet
-from .views import login
+from .views import SubscriberViewSet 
+from .views import login, register
 from django.conf.urls import url
 
 
 router = SimpleRouter()
-router.register("subscribers",SubscriberViewSet)
+router.register('subscribers',SubscriberViewSet)
+
 
 
 
 urlpatterns = router.urls + [
     
-    url(r'^login', login)
+    url(r'^login', login),
+    url(r'^register',register)
+    
+   
 ]
