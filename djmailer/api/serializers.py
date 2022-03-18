1de0758ce178d74c337e6bdd71afcb1b73d71d90
Sandeepsubramaniyan <sandeekathiravan@gmail.com>
2022-03-18 11:13:13 +0530

    registerserialzer created

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -1,8 +1,41 @@
-from rest_framework import serializers
-from .models import Subscriber
+from django.contrib.auth.models import User
+from rest_framework import serializers , validators
+from .models import  Subscriber
+
 
 class SubscriberSerializer(serializers.ModelSerializer):
-    class Meta:
+    
+    class Meta :
         model = Subscriber
-        fields  = "__all__"
+        fields = '__all__'
+        
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+        extra_kwargs = {
+            'password' :{'write_only': True},
+            'email':{
+                'required':True,
+                'allow_blank':False,
+                'validators': [
+                    validators.UniqueValidator(
+                        User.objects.all(),"A user with that email already exists"
+                        
+                    )
+                ]
+                
+            }
+        }
+        
+    
+    def create(self,validated_data):
+        
+        return User.objects.create_user(**validated_data)
+
+
+            
+
         
--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import SubscriberSerializer
+from .serializers import SubscriberSerializer , RegisterSerializer
 from .models import Subscriber
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
@@ -8,12 +8,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 
+
 class SubscriberViewSet(ModelViewSet):
     serializer_class = SubscriberSerializer
     queryset = Subscriber.objects.all()
     permission_classes = (IsAuthenticated,)
     
-    
+
 @api_view(["POST"])  
 def login(request):
     username=request.data.get("username")
@@ -24,6 +25,37 @@ def login(request):
     if not user:
         return Response({"error": "Login failed"},status = HTTP_401_UNAUTHORIZED)
     
-    token= Token.objects.get_or_create(user=user)
+    (token,created)= Token.objects.get_or_create(user=user)
     return Response({"token": token.key})
+
+
+    
+@api_view(["POST"])  
+def register(request):
+    serializer= RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    
+    user= serializer.save() 
+    
+    (token)= Token.objects.create(user=user)
+    return Response({
+        'user_info': {
+            'username':user.username,
+            'email':user.email,
+            },
+        "token": token.key
+        })
+    
+    
+    
+    
+
+            
+            
+            
+        
+
+
+
+
 
